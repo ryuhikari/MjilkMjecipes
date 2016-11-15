6c907aa2433b5b07c754422e4f2bd3bfed8e4c0e
Fernando <fernandovillab@gmail.com>
2016-11-15 19:18:45 +0100

    Added Log In Activity
    Its layout still need to be filled and it must also be the launch
    activity.

---
+++ app/src/main/java/se/ju/taun15a16/group5/mjilkmjecipes/LogInActivity.java
@@ -0,0 +1,13 @@
+package se.ju.taun15a16.group5.mjilkmjecipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
